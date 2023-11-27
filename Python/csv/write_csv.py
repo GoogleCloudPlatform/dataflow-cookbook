beb25bda380c663655b7467fcfa00b495d756e94
vitaly.terentyev <vitaly.terentyev@akvelon.com>
2023-11-27 18:34:43 +0400

    Add Python Csv pipelines for read/write

---
+++ Python/csv/write_csv.py
@@ -0,0 +1,66 @@
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import logging
+import apache_beam as beam
+
+from apache_beam import Create
+from apache_beam import Map
+from apache_beam.io.textio import WriteToCsv
+from apache_beam.options.pipeline_options import PipelineOptions
+
+
+class CsvOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument(
+            '--file_path',
+            default="gs://quick_test_dataflow/test-names.csv",
+            help='Csv file path'
+        )
+
+
+def run():
+    """
+    This pipeline shows how to write to Csv file.
+    """
+
+    options = CsvOptions()
+
+    with beam.Pipeline(options=options) as p:
+
+        elements = [
+            (1, "Charles"),
+            (2, "Alice"),
+            (3, "Bob"),
+            (4, "Amanda"),
+            (5, "Alex"),
+            (6, "Eliza")
+        ]
+
+        output = (p | 'Create' >> Create(elements)
+                    | 'Map to Row' >> Map(map_to_row)
+                    | "Write to Csv file" >> WriteToCsv(
+                        path=options.file_path
+                    ))
+
+
+def map_to_row(element):
+
+    return beam.Row(id=element[0], name=element[1])
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    run()
